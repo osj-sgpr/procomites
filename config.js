77e1345c6400d890d50a98494e36785939618419
OSJ <otaciliosilveira@gmail.com>
2026-02-22 12:09:01 -0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-/ Preencha com a URL do Web App do Google Apps Script (deploy)
-// Exemplo: https://script.google.com/macros/s/XXXX/exec
-window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzPGdYJ6kYEPPDCZUaoCqyxoDvtlwQOTunvvkPP2RHjMg0TzvflUQdfMcGxE-hSTgT3/exec";
